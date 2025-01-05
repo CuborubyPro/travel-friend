7592bf3731d500314e4da03b55ecbe97890a2cb0
CuborubyPro <monkey@donkey.com>
2025-01-05 21:46:16 +0100

    Add Home Page

---
+++ src/app/pages/home.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+export default function LandingPage(){
+    const navigate = useNavigate()
+
+    return (
+        <div>
+            Hola Mundo
+        </div>
+    )
+}
